69ef11c1a20c1b31704f67253273829b15481124
Christopher <bopherchan@gmail.com>
2023-01-31 16:53:07 -0500

    completed arrow

---
+++ index.js
@@ -0,0 +1,9 @@
+let num1 = 2000
+let num2 = 100
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+const square = (num) => num ** 2
+
+const add = (something1, something2) => something1 + something2 
